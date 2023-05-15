81cfa3f9a46e392381b77cc302b48b394d7f96b9
Anthony Dillon <me@anthonydillon.com>
2023-05-15 15:10:51 +0100

    Bump the version to 1.1.0

--- application-review.user.js
+++ application-review.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greenhouse Application Review
 // @namespace    https://canonical.com/
-// @version      1.0.2
+// @version      1.1.0
 // @author       Canonical's workplace engineering team
 // @description  Add shortcut buttons to application review page
 // @homepage     https://github.com/canonical/greenhouse-browser-scripts
