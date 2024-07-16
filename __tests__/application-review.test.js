afb0e817d64d848e22fb6ff6f384513c285b6cd2
Nathan Clairmonte <nathan.clairmonte@canonical.com>
2024-07-16 17:53:25 -0400

    fix rejection buttons (#62)
    * update application-review script to check for reject modal and dropdown options before proceeding
    
    * update application-review test accordingly
    
    * add comments to application-review script

--- __tests__/application-review.test.js
+++ __tests__/application-review.test.js
@@ -3,6 +3,7 @@ import { JSDOM } from "jsdom";
 
 describe("Application review test", () => {
     let testContainer;
+    let document;
 
     beforeAll(() => {
         const dom = new JSDOM(
@@ -12,7 +13,6 @@ describe("Application review test", () => {
             <head></head>
               <body>
                 <div id="test-container">
-                  <div id="reject-modal"/>
                   <button data-provides="reject">
                 </div>
                 <script>
@@ -30,7 +30,8 @@ describe("Application review test", () => {
             }
         );
 
-        testContainer = dom.window.document.getElementById("test-container");
+        document = dom.window.document;
+        testContainer = document.getElementById("test-container");
     });
 
     it("adds action bar elements to the UI", () => {
@@ -41,12 +42,20 @@ describe("Application review test", () => {
         expect(children).toHaveLength(4);
     });
 
-    it("handles reject", () => {
+    it("handles reject", async () => {
         const rejectionContainer = testContainer.querySelector(".rejection");
         const rejectButton = rejectionContainer.firstElementChild;
         userEvent.click(rejectButton);
+
+        // simulate addition of reject modal to the DOM
+        const rejectModal = document.createElement("div");
+        rejectModal.id = "reject-modal";
+        testContainer.append(rejectModal);
+
+        // wait for the reject modal to be detected and processed
+        await new Promise(resolve => setTimeout(resolve, 200));
+
         const spinner = rejectionContainer.querySelector(".spinner");
-        const rejectModal = testContainer.querySelector("#reject-modal");
 
         expect(rejectModal).toHaveClass("hide-modal");
         expect(spinner).toBeInTheDocument();
--- application-review.user.js
+++ application-review.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Greenhouse Application Review
 // @namespace    https://canonical.com/
-// @version      1.1.1
+// @version      1.1.2
 // @author       Canonical's workplace engineering team
 // @description  Add shortcut buttons to application review page
 // @homepage     https://github.com/canonical/greenhouse-browser-scripts
@@ -44,62 +44,80 @@
         addSpinner(element);
         setLoading();
         rejectBtnEl.click();
-        const rejectModal = document.querySelector("#reject-modal");
-        rejectModal.classList.add("hide-modal");
-        const sendEmailInput = rejectModal.querySelector("#send-email");
-        if (!sendEmailInput.checked) {
-            sendEmailInput.click();
-        }
-        const rejectButton = rejectModal.querySelector(
-            'a[title="Reject this candidate"]'
-        );
-        let reasonSelector = rejectModal.querySelector(
-            "[data-provides='reject-reason-dropdown'] .Select-value-label"
-        );
-        if (!reasonSelector) {
-            reasonSelector = rejectModal.querySelector(
-                "[data-provides='reject-reason-dropdown'] .Select-placeholder"
-            );
-        }
-        reasonSelector.dispatchEvent(keyDown);
-        const reasonOption = rejectModal.querySelectorAll(
-            ".Select-menu-outer .Select-option"
-        );
-        reasonOption.forEach(function (option) {
-            if (option.getAttribute("aria-label") === reason) {
-                option.dispatchEvent(mouseDown);
-            }
-        });
-        let fromInput;
-        let subjectInput;
-        const dropdownContainers = rejectModal.querySelectorAll(
-            '#reject-modal .sl-dropdown-container'
-        );
-        dropdownContainers.forEach((input) => {
-            if (input.previousElementSibling?.innerText === "From") {
-                fromInput = input;
-            }
-        });
-        const inputs = rejectModal.querySelectorAll(
-            '#reject-modal input[type="text"]'
-        );
-        inputs.forEach((input) => {
-            if (input.previousElementSibling?.innerText === "Subject") {
-                subjectInput = input;
-            }
-        });
-        const rejectChecker = setInterval(() => {
-            if (subjectInput.value !== "") {
-                clearInterval(rejectChecker);
-                setFromToNoReply(fromInput)
-                rejectButton.click();
-                setEnabled();
-                setTimeout(
-                    () => rejectModal.classList.remove("hide-modal"),
-                    500
+
+        // check for reject modal before continuing
+        const rejectModalChecker = setInterval(() => {
+            const rejectModal = document.querySelector("#reject-modal");
+            if (rejectModal) {
+                clearInterval(rejectModalChecker);
+                rejectModal.classList.add("hide-modal");
+                const sendEmailInput = rejectModal.querySelector("#send-email");
+                if (!sendEmailInput.checked) {
+                    sendEmailInput.click();
+                }
+                const rejectButton = rejectModal.querySelector(
+                    'a[title="Reject this candidate"]'
                 );
+                let reasonSelector = rejectModal.querySelector(
+                    "[data-provides='reject-reason-dropdown'] .Select-value-label"
+                );
+                if (!reasonSelector) {
+                    reasonSelector = rejectModal.querySelector(
+                        "[data-provides='reject-reason-dropdown'] .Select-placeholder"
+                    );
+                }
+                reasonSelector.dispatchEvent(keyDown);
+
+                // check for dropdown menu before continuing
+                const dropdownMenuChecker = setInterval(() => {
+                    const dropdownMenu = rejectModal.querySelector(
+                        ".Select-menu-outer"
+                    );
+                    if (dropdownMenu) {
+                        clearInterval(dropdownMenuChecker);
+                        const reasonOptions = rejectModal.querySelectorAll(
+                            ".Select-menu-outer .Select-option"
+                        );
+                        reasonOptions.forEach(function (option) {
+                            if (option.getAttribute("aria-label") === reason) {
+                                option.dispatchEvent(mouseDown);
+                            }
+                        });
+                    }
+                }, 100);
+
+                let fromInput;
+                let subjectInput;
+                const dropdownContainers = rejectModal.querySelectorAll(
+                    '#reject-modal .sl-dropdown-container'
+                );
+                dropdownContainers.forEach((input) => {
+                    if (input.previousElementSibling?.innerText === "From") {
+                        fromInput = input;
+                    }
+                });
+                const inputs = rejectModal.querySelectorAll(
+                    '#reject-modal input[type="text"]'
+                );
+                inputs.forEach((input) => {
+                    if (input.previousElementSibling?.innerText === "Subject") {
+                        subjectInput = input;
+                    }
+                });
+                const rejectChecker = setInterval(() => {
+                    if (subjectInput.value !== "") {
+                        clearInterval(rejectChecker);
+                        setFromToNoReply(fromInput);
+                        rejectButton.click();
+                        setEnabled();
+                        setTimeout(
+                            () => rejectModal.classList.remove("hide-modal"),
+                            500
+                        );
+                    }
+                }, 500);
             }
-        }, 500);
+        }, 100);
     }
 
     // State utilities
